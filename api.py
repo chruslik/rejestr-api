66571819a9a73999e2934f2955e7b08bf26843a0
chruslik <chruslik@gmail.com>
2025-07-26 23:13:03 +0200

    Wersja 3.0.7

--- api.py
+++ api.py
@@ -238,4 +238,4 @@ def get_slowniki():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
