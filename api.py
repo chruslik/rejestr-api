ca7b9da7da4ca86e3493f2e35f84acc19f264bb3
chruslik <chruslik@gmail.com>
2025-07-26 22:59:46 +0200

    Wersja 3.0.7

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import os  # ← to jest wymagane
 
 app = Flask(__name__)
 CORS(app)
