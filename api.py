4813581cfb9ee4eb52d911a5b657904737cfb09e
chruslik <chruslik@gmail.com>
2025-07-26 23:40:28 +0200

    Wersja 3.1.2

--- api.py
+++ api.py
@@ -249,4 +249,4 @@ if __name__ == "__main__":
     except Exception as e:
         print("Błąd podczas inicjalizacji bazy:", e)
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
