9425fdc28d6cb7c6360cb2473e31b1231c8291a1
chruslik <chruslik@gmail.com>
2025-07-28 14:43:43 +0200

    Wersja 3.2.2

--- api.py
+++ api.py
@@ -251,11 +251,5 @@ def test_db():
 
 
 if __name__ == "__main__":
-    print("Inicjalizacja bazy danych...")
-    try:
-        init_db()
-        print("Baza danych została poprawnie zainicjalizowana.")
-    except Exception as e:
-        print("Błąd podczas inicjalizacji bazy:", e)
-
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+       port = int(os.environ.get("PORT", 5000))
+       app.run(host="0.0.0.0", port=port)
