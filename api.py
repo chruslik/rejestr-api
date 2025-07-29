3b660670baa7f0fca50e0dea40b3722c71e8fa64
chruslik <chruslik@gmail.com>
2025-07-29 11:38:40 +0200

    Wersja 3.3.5

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ import traceback
 app = Flask(__name__)
 CORS(app)
 
-DATABASE_URL = "ostgresql://postgres.njnotdsifzblpnlfulau:IcJut0sNYnMPcdbf@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql://postgres.njnotdsifzblpnlfulau:IcJut0sNYnMPcdbf@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
 
 
 
